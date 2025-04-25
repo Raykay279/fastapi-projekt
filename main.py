6286b246fdf0faa51d2d1aaa21d44b143bca850b
Raykay279 <rakhosch@gmail.com>
2025-04-25 18:02:35 +0200

    Fast API stand Buch hinzufügen

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def startseite():
-    return {"Nachricht": "Herzlich Willkommen auf meinem FastAPI-Server!"}
+# Datenmodell
+class Buch(BaseModel):
+    titel: str
+    autor: str
+    jahr: int
+    isbn: str
+
+# DB im Speicher
+buecher_liste: List[Buch] = []
+
+# Get-Route: alle Bücher anzeigen
+@app.get("/buecher", response_model=List[Buch])
+def get_buecher():
+    return buecher_liste
+
+# Post-Route: Ein Buch hinzufügen
+@app.post("/buecher", response_model=Buch)
+def add_buch(buch: Buch):
+    buecher_liste.append(buch)
+    return buch
